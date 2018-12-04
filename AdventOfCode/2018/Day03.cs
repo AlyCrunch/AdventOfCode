4e5ce5cb435d598e73546a4efbf7a60a7d61e2dc
Alison JAROSZ <alison.jarosz@viacesi.fr>
2018-12-04 14:50:36 +0100

    Removes useless class

--- AdventOfCode/2018/Day03.cs
+++ AdventOfCode/2018/Day03.cs
@@ -21,20 +21,27 @@ namespace AdventOfCode._2018
             Console.WriteLine(Answer(file));
         }
 
-        public static object Answer(string[] inputs)
+        private static object Answer(string[] inputs)
         {
             Dictionary<(int X, int Y), HashSet<int>> fabrics = new Dictionary<(int X, int Y), HashSet<int>>();
             HashSet<int> all_claims = new HashSet<int>();
             foreach (var input in inputs)
             {
-                Fabric fabric = new Fabric(input);
-                all_claims.Add(fabric.ID);
-                foreach (var (X, Y) in fabric.Coords)
+                int ID = GetIDFromString(input);
+                var origin = GetCoordFromString(GetValuePart(input)[0].Split(','));
+                var size = GetCoordFromString(GetValuePart(input)[1].Split('x'));
+                
+                all_claims.Add(ID);
+
+                for (int y = 0; y < size.Y; y++)
                 {
-                    if (fabrics.ContainsKey((X, Y)))
-                        fabrics[(X, Y)].Add(fabric.ID);
-                    else
-                        fabrics.Add((X, Y), new HashSet<int>() { fabric.ID });
+                    for (int x = 0; x < size.X; x++)
+                    {
+                        if (fabrics.ContainsKey((origin.X + x, origin.Y + y)))
+                            fabrics[(origin.X + x, origin.Y + y)].Add(ID);
+                        else
+                            fabrics.Add((origin.X + x, origin.Y + y), new HashSet<int>() { ID });
+                    }
                 }
             }
 
@@ -46,46 +53,9 @@ namespace AdventOfCode._2018
             var ID_claimed_not_overlapped = all_claims.Except(claimed_id).First();
             return $"Overlaped inches sq. : {square_inches} & not overlapped ID of claim : {ID_claimed_not_overlapped}";
         }
+
+        private static int GetIDFromString(string str) => int.Parse(str.Split(" @ ")[0].Split("#", StringSplitOptions.RemoveEmptyEntries).First());
+        private static string[] GetValuePart(string str) => str.Split(" @ ")[1].Split(": ");
+        private static (int X, int Y) GetCoordFromString(string[] arr) => (int.Parse(arr[0]), int.Parse(arr[1]));
     }
-
-    //Pretty useless class
-    public class Fabric
-    {
-        public int ID { get; set; }
-        public (int X, int Y) Origin { get; set; }
-        public (int X, int Y) Size { get; set; }
-        public List<(int X, int Y)> Coords { get; set; }
-
-        public Fabric(string str) //#1 @ 1,3: 4x4
-        {
-
-            ID = int.Parse(str.Split(" @ ")[0].Split("#", StringSplitOptions.RemoveEmptyEntries).First());
-            string[] useful = str.Split(" @ ")[1].Split(": ");
-
-            Origin = GetCoordFromString(useful[0].Split(','));
-            Size = GetCoordFromString(useful[1].Split('x'));
-            Coords = CalculateAllCoordsT(Origin, Size);
-        }
-
-        public (int, int) GetCoordFromString(string[] arr)
-        {
-            return (int.Parse(arr[0]), int.Parse(arr[1]));
-        }
-
-        private List<(int X, int Y)> CalculateAllCoordsT((int X, int Y) origin, (int X, int Y) size)
-        {
-            List<(int X, int Y)> coords = new List<(int X, int Y)>();
-
-            for (int y = 0; y < size.Y; y++)
-            {
-                for (int x = 0; x < size.X; x++)
-                {
-                    coords.Add((x + origin.X, y + origin.Y));
-                }
-            }
-
-            return coords;
-        }
-    }
-
 }
