36b410149d56d1c08cbdf504d62f92aac6c50bfa
Alison <alison.jarosz@viacesi.fr>
2021-12-02 00:07:23 +0100

    Factorize Day 1

--- Days/01.cs
+++ Days/01.cs
@@ -4,30 +4,12 @@ namespace Days
 {
     public static class SonarSweep
     {
-        public static int GetDepthNumberInc(int[] values)
-        {
-            var increased = 0;
-            int memory = values[0];
+        public static int GetDepthNumberInc(IEnumerable<int> values)
+            => values.Skip(1).Zip(values).Count(x => x.First > x.Second);
 
-            values.Skip(1).ToList().ForEach(x =>
-            {
-                if (x > memory) increased++;
-                memory = x;
-            });
-
-            return increased;
-        }
-
-        public static int GetDepthNumberIncGroupedBy3(int[] values)
-        {
-            var increased = 0;
-            for (int i = 1; i < values.Length - 2; i++)
-            {
-                var a = values[i - 1] + values[i] + values[i + 1];
-                var b = values[i] + values[i + 1] + values[i + 2];
-                if (b > a) increased++;
-            }
-            return increased;
-        }
+        public static int GetDepthNumberIncGroupedBy3(IEnumerable<int> values)
+            => GetDepthNumberInc(values.Skip(2)
+                .Zip(values.Skip(1).Zip(values))
+                .Select(x => x.First + x.Second.First + x.Second.Second));
     }
 }
