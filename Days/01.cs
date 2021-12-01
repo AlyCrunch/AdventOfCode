bc728723c22f4c8f73f41f40a532ecc1308320ec
Alison <alison.jarosz@viacesi.fr>
2021-12-01 12:10:19 +0100

    Solves Day 1

--- Days/01.cs
+++ Days/01.cs
@@ -1,17 +1,33 @@
-﻿namespace Days
+﻿using System.Linq;
+
+namespace Days
 {
-    public static class First
+    public static class SonarSweep
     {
-        public static string OutputFirst(string filename)
+        public static int GetDepthNumberInc(int[] values)
         {
-            var sample = Helpers.ReadFileAsInteger(filename);
-            return string.Empty;
+            var increased = 0;
+            int memory = values[0];
+
+            values.Skip(1).ToList().ForEach(x =>
+            {
+                if (x > memory) increased++;
+                memory = x;
+            });
+
+            return increased;
         }
 
-        public static string OutputSecond(string filename)
+        public static int GetDepthNumberIncGroupedBy3(int[] values)
         {
-            var sample = Helpers.ReadFileAsInteger(filename);
-            return string.Empty;
+            var increased = 0;
+            for (int i = 1; i < values.Length - 2; i++)
+            {
+                var a = values[i - 1] + values[i] + values[i + 1];
+                var b = values[i] + values[i + 1] + values[i + 2];
+                if (b > a) increased++;
+            }
+            return increased;
         }
     }
 }
