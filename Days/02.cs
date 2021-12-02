c4a0e3450941690bdd9f2fb49ce9f94aa4b35557
Alison <alison.jarosz@viacesi.fr>
2021-12-02 19:41:59 +0100

    Factorizes Day 2

--- Days/02.cs
+++ Days/02.cs
@@ -13,29 +13,17 @@ namespace Days
 
         public static (int depth, int hPosition) GetFinalPosition(IEnumerable<string> datas)
             => FormatData(datas)
-                .Select(x => x.name switch
-                {
-                    "forward" => (0, x.nb),
-                    "down" => (x.nb, 0),
-                    "up" => (-1 * x.nb, 0),
-                    _ => (0, 0),
-                })
-                .Aggregate((depth: 0, hPosition: 0), (sum, item) => (sum.depth + item.Item1, sum.hPosition + item.Item2));
+                .Aggregate((depth: 0, hPosition: 0), (sum, item) =>
+                ((item.name != "forward") ? sum.depth + ((item.name != "up") ? item.nb : item.nb * -1) : sum.depth,
+                (item.name == "forward") ? sum.hPosition + item.nb : sum.hPosition));
 
         public static (int depth, int hPosition, int aim) GetFinalPositionWithAim(IEnumerable<string> datas)
             => FormatData(datas)
-                .Select(x => x.name switch
-                {
-                    "forward" => (0, x.nb),
-                    "down" => (x.nb, 0),
-                    "up" => (-1 * x.nb, 0),
-                    _ => (0, 0),
-                })
                 .Aggregate((depth: 0, hPosition: 0, aim: 0), (sum, item) =>
                        (
-                       (item.Item2 != 0 && sum.aim != 0) ? sum.depth + sum.aim * item.Item2 : sum.depth,
-                       (item.Item2 != 0) ? item.Item2 + sum.hPosition : sum.hPosition,
-                       sum.aim + item.Item1
+                       (item.name == "forward") ? sum.depth + sum.aim * item.nb : sum.depth,
+                       (item.name == "forward") ? item.nb + sum.hPosition : sum.hPosition,
+                       (item.name != "forward") ? sum.aim + ((item.name != "up") ? item.nb : item.nb * -1) : sum.aim
                        ));
 
     }
