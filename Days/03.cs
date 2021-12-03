3f8b11f584f9d047740cafde068a4be8e237f799
Alison <alison.jarosz@viacesi.fr>
2021-12-03 18:59:57 +0100

    Solve first part day 3

---
+++ Days/03.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Days
+{
+    public static class BinaryDiagnostic
+    {
+
+        public static int[][] SwapArr(IEnumerable<string> datas)
+        {
+            var nbBits = datas.First().Length;
+            var swapped = new int[nbBits][];
+            Parallel.For(0, nbBits,
+                i => swapped[i] = datas.Select(c => c[i] - '0').ToArray());
+            return swapped;
+        }
+
+        public static int GetConsumption(IEnumerable<string> datas)
+        {
+            var gamma = GetGammaRateWithSwap(datas);
+            return Convert.ToInt32(gamma, 2) *
+                Convert.ToInt32(string.Concat(gamma.Select(x => (x == '0') ? '1' : '0')), 2);
+        }
+
+        private static string GetGammaRateWithSwap(IEnumerable<string> datas)
+            => string.Join("", SwapArr(datas)
+                .Select(x => (x.Sum() > datas.Count() / 2) ? '1' : '0'));
+    }
+}
---
+++ Tests/Day03.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+using Days;
+
+namespace Tests
+{
+    public class Day03
+    {
+
+        private static readonly string[] test =
+            {
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"};
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var c = BinaryDiagnostic.GetConsumption(test);
+            Assert.Equal(198, c);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Helpers.ReadFile("Inputs\\03.txt");
+            var c = BinaryDiagnostic.GetConsumption(dataset);
+            Assert.Equal(4191876, c);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+        }
+    }
+}
