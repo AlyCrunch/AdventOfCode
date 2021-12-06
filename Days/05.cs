0fdb7f7607606dbaa2c9f7a9228a4f15d0bb9257
Alison <alison.jarosz@viacesi.fr>
2021-12-06 01:35:28 +0100

    Solve Day 5

---
+++ Days/05.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+
+namespace Days
+{
+    public static class HydrothermalVenture
+    {
+        public static int CountOverlaps(IEnumerable<string> datas)
+        => GetVerticalsAndHorizontals(FormatDatas(datas))
+            .SelectMany(vent => GetPositions(vent))
+            .GroupBy(x => x)
+            .Where(x => x.Count() >= 2)
+            .Count();
+
+        public static int CountOverlapsWithDiagonals(IEnumerable<string> datas)
+        => GetVerticalsHorizontalsDiagonals(FormatDatas(datas))
+            .SelectMany(vent => GetPositions(vent))
+            .GroupBy(x => x)
+            .Where(x => x.Count() >= 2)
+            .Count();
+
+        public static IEnumerable<((int x, int y) from, (int x, int y) to)> FormatDatas(IEnumerable<string> datas)
+         => datas.Select(x => x.Split(" -> ")
+         .Select(y => y.Split(',')
+         .Select(z => int.Parse(z))))
+            .Select(x => ((x.ElementAt(0).ElementAt(0), x.ElementAt(0).ElementAt(1)),
+                          (x.ElementAt(1).ElementAt(0), x.ElementAt(1).ElementAt(1))));
+
+        public static IEnumerable<((int x, int y) from, (int x, int y) to)> GetVerticalsAndHorizontals(IEnumerable<((int x, int y) from, (int x, int y) to)> list)
+            => list.Where(x => (x.from.x == x.to.x) || (x.from.y == x.to.y));
+
+        public static IEnumerable<((int x, int y) from, (int x, int y) to)> GetVerticalsHorizontalsDiagonals(IEnumerable<((int x, int y) from, (int x, int y) to)> list)
+            => list.Where(x => Math.Abs(x.from.x - x.to.x) == Math.Abs(x.from.y - x.to.y) 
+                            || (x.from.x == x.to.x) || (x.from.y == x.to.y));
+
+        public static IEnumerable<(int x, int y)> GetPositions(((int x, int y) from, (int x, int y) to) point)
+        {
+            var a = point.from;
+            var b = point.to;
+            var rangeX = GetRange(a.x, b.x, Math.Abs(a.y - b.y));
+            var rangeY = GetRange(a.y, b.y, Math.Abs(a.x - b.x));
+
+            return rangeX.Zip(rangeY).Select(x => (x.First,x.Second));
+        }
+
+        private static IEnumerable<int> GetRange(int from, int to, int maxOther)
+            => (from == to) ? Enumerable.Range(0, maxOther + 1).Select(_ => from) :
+                (to > from) ? Enumerable.Range(from, to + 1 - from) : Enumerable.Range(to, from + 1 - to).Reverse();
+    }
+}
---
+++ Tests/Day05.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using Days;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class Day05
+    {
+        private static readonly string[] test =
+        {
+            "0,9 -> 5,9",
+            "8,0 -> 0,8",
+            "9,4 -> 3,4",
+            "2,2 -> 2,1",
+            "7,0 -> 7,4",
+            "6,4 -> 2,0",
+            "0,9 -> 2,9",
+            "3,4 -> 1,4",
+            "0,0 -> 8,8",
+            "5,5 -> 8,2"
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var x = HydrothermalVenture.CountOverlaps(test);
+            Assert.Equal(5, x);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Helpers.ReadFile("Inputs\\05.txt");
+            var x = HydrothermalVenture.CountOverlaps(dataset);
+            Assert.Equal(5608, x);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var x = HydrothermalVenture.CountOverlapsWithDiagonals(test);
+            Assert.Equal(12, x);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Helpers.ReadFile("Inputs\\05.txt");
+            var x = HydrothermalVenture.CountOverlapsWithDiagonals(dataset);
+            Assert.Equal(20299, x);
+        }
+    }
+}
