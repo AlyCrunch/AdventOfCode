9d65817449ad5036751eb6b680256c2a17368030
Alison <alison.jarosz@viacesi.fr>
2021-12-07 23:08:50 +0100

    Solve Day 7

---
+++ Days/07.cs
@@ -0,0 +1,23 @@
+﻿namespace Days
+{
+    public static class TheTreacheryofWhales
+    {
+        public static int GetLeastFuelPossible(string datas)
+        {
+            var pos = datas.Split(',').Select(x => int.Parse(x));
+            List<int> result = new();
+            Parallel.For(pos.Min(), pos.Max(),
+                i => result.Add(pos.Aggregate(0, (sum, item) => sum + Math.Abs(item - i))));
+            return result.Min(x => x);
+        }
+        public static int GetLeastFuelPossibleInc(string datas)
+        {
+            var pos = datas.Split(',').Select(x => int.Parse(x));
+            List<int> result = new();
+            Parallel.For(pos.Min(), pos.Max(),
+                i => result.Add(pos.Aggregate(0, (sum, item)
+                => sum + (Math.Abs(item - i) * (Math.Abs(item - i) + 1) / 2))));
+            return result.Min(x => x);
+        }
+    }
+}
