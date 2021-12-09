76e0f8464f810b563feb044ac68620a5b2ec7d5a
AlyCrunch <alison.jarosz@viacesi.fr>
2021-12-09 18:31:27 +0100

    Solve day 9

---
+++ Days/09.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Days
+{
+    public static class SmokeBasin
+    {
+        public static int GetRiskLevel(string[] raw)
+            => GetLowPoint(GetMap(raw)).Select(x => x.height + 1).Sum();
+        public static IEnumerable<(int x, int y, int height)> GetLowPoint(int[][] map)
+        {
+            var lowPoints = new List<(int x, int y, int height)>();
+
+            for (int x = 0; x < map.Length; x++)
+            {
+                for (int y = 0; y < map[x].Length; y++)
+                {
+                    if (IsLowPoint(map, x, y))
+                        lowPoints.Add((x, y, map[x][y]));
+                }
+            }
+            return lowPoints;
+        }
+
+        public static bool IsLowPoint(int[][] map, int x, int y)
+            => map[x][y] < GetValue(map, x - 1, y)
+            && map[x][y] < GetValue(map, x + 1, y)
+            && map[x][y] < GetValue(map, x, y - 1)
+            && map[x][y] < GetValue(map, x, y + 1);
+
+        public static int GetValue(int[][] map, int x, int y)
+            => (x < 0 || y < 0 || x >= map.Length || y >= map[x].Length) ? 10 : map[x][y];
+
+        public static int[][] GetMap(string[] raw)
+            => raw.Select(r => r.Select(x => x - '0').ToArray()).ToArray();
+    }
+}
---
+++ Tests/Day09.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using Days;
+
+namespace Tests
+{
+    public class Day09
+    {
+        private static readonly string[] test =
+        {
+            "2199943210",
+            "3987894921",
+            "9856789892",
+            "8767896789",
+            "9899965678"
+        };
+
+        [Fact]
+        public void Debug()
+        {
+        }
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var x = SmokeBasin.GetRiskLevel(test);
+            Assert.Equal(15, x);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Helpers.ReadFile("Inputs\\09.txt");
+            var x = SmokeBasin.GetRiskLevel(dataset);
+            Assert.Equal(462, x);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+        }
+    }
+}
