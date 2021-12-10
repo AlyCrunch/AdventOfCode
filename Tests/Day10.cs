8fe8d0f00bb423a77b4bfb9cb3784aa2cca80cba
AlyCrunch <alison.jarosz@viacesi.fr>
2021-12-10 18:57:11 +0100

    Solve Day 10

---
+++ Tests/Day10.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using Days;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class Day10
+    {
+        private static readonly string[] test =
+        {
+            "[({(<(())[]>[[{[]{<()<>>",
+            "[(()[<>])]({[<{<<[]>>(",
+            "{([(<{}[<>[]}>{[]{[(<()>",
+            "(((({<>}<{<{<>}{[]{[]{}",
+            "[[<[([]))<([[{}[[()]]]",
+            "[{[{({}]{}}([{[{{{}}([]",
+            "{<[[]]>}<{[{[{[]{()[[[]",
+            "[<(<(<(<{}))><([]([]()",
+            "<{([([[(<>()){}]>(<<{{",
+            "<{([{{}}[<[[[<>{}]]]>[]]"
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var x = SyntaxScoring.GetScore(test);
+            Assert.Equal(26397, x);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Helpers.ReadFile("Inputs\\10.txt");
+            var x = SyntaxScoring.GetScore(dataset);
+            Assert.Equal(392421, x);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var x = SyntaxScoring.GetMiddleScore(test);
+            Assert.Equal(288957, x);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Helpers.ReadFile("Inputs\\10.txt");
+            var x = SyntaxScoring.GetMiddleScore(dataset);
+            Assert.Equal(2769449099, x);
+        }
+    }
+}
