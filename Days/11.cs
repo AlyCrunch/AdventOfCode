17241986b85a945fc2178a535ee0735a16afa4c1
AlyCrunch <alison.jarosz@viacesi.fr>
2021-12-11 17:10:07 +0100

    Add files for Day 11

---
+++ Days/11.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Days
+{
+    public static class DumboOctopus
+    {
+        public static int GetCountOfFlashes(string[] input, int steps = 100)
+        {
+            var octopus = Format(input);
+            var states = new List<Dictionary<(int x, int y), int>>();
+
+            for (int i = 0; i < steps; i++)
+            {
+                Debug.WriteLine($"\nStep : {i + 1}\n");
+                states.Add(GetNextStep(octopus));
+                ToString(states.Last()).ToList().ForEach(line => Debug.WriteLine($"{line}"));
+            }
+
+            return states.Sum(x => x.Count(y => y.Value == 0));
+        }
+
+        private static Dictionary<(int x, int y), int> GetNextStep(Dictionary<(int x, int y), int> octopus)
+        {
+            octopus = octopus.ToDictionary(key => key.Key, v => v.Value + 1);
+            var flashedOctopus = MakeFlash(octopus, new Dictionary<(int x, int y), int>());
+
+            return flashedOctopus.ToDictionary(key => key.Key, v => (v.Value > 9) ? 0 : v.Value);
+        }
+
+        private static Dictionary<(int x, int y), int> MakeFlash
+            (Dictionary<(int x, int y), int> octopus, Dictionary<(int x, int y), int> flashed, int flashAt = 9)
+        {
+            var newFlashed = octopus.Where(x => x.Value > flashAt && !flashed.ContainsKey(x.Key));
+            
+            if (!newFlashed.Any()) return octopus;
+
+
+            foreach (var kv in newFlashed)
+            {
+                GetAdjacentsOctopuses(kv.Key).ToList().ForEach(x => { if (octopus.ContainsKey(x)) octopus[x] = octopus[x] + 1; } );
+                flashed.Add(kv.Key, kv.Value);
+            }
+
+            return MakeFlash(octopus, flashed);
+        }
+
+        private static IEnumerable<(int x, int y)> GetAdjacentsOctopuses((int x, int y) coord)
+            => new (int x, int y)[]
+            {
+                (coord.x - 1, coord.y - 1),
+                (coord.x - 1, coord.y),
+                (coord.x - 1, coord.y + 1),
+                (coord.x, coord.y - 1),
+                (coord.x, coord.y + 1),
+                (coord.x + 1, coord.y - 1),
+                (coord.x + 1, coord.y),
+                (coord.x + 1, coord.y + 1)
+            };
+
+        private static IEnumerable<string> ToString(Dictionary<(int x, int y), int> state)
+            => state.GroupBy(x => x.Key.x).Select(o => string.Join("", o.OrderBy(v => v.Key.y).Select(v => v.Value)));
+
+        private static Dictionary<(int x, int y), int> Format(string[] input)
+            => input.Select((line, x) => line.Select((value, y) => ((x, y), value - '0')))
+                            .SelectMany(x => x)
+                            .ToDictionary(key => key.Item1, value => value.Item2);
+    }
+}
---
+++ Tests/Day11.cs
@@ -0,0 +1,57 @@
+﻿using Xunit;
+using Days;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class Day11
+    {
+        private static readonly string[] small =
+        {
+            "11111",
+            "19991",
+            "19191",
+            "19991",
+            "11111"
+        };
+        private static readonly string[] test =
+        {
+            "5483143223",
+            "2745854711",
+            "5264556173",
+            "6141336146",
+            "6357385478",
+            "4167524645",
+            "2176841721",
+            "6882881134",
+            "4846848554",
+            "5283751526"
+        };
+
+
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            //var y = DumboOctopus.GetCountOfFlashes(test);
+            //Assert.Equal(1656, y);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            //var dataset = Helpers.ReadFile("Inputs\\11.txt");
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+        }
+    }
+}
