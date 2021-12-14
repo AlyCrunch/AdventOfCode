bbfe0b960a9f7a40f14dc7decb61fb7c65eff590
Alison <alison.jarosz@viacesi.fr>
2021-12-14 01:27:34 +0100

    [2017] Add and fix day 07

--- Days/2017/07.cs
+++ Days/2017/07.cs
@@ -1,28 +1,24 @@
-﻿namespace Days
+﻿namespace Days._2017
 {
     public static class RecursiveCircus
     {
-        private static int CorrectNodeValue;
-
         public static string GetNameBottom(string[] lines)
-            => GetParent(lines).Name;
+            => GetTree(lines).Name;
 
-        public static int Part2Tree(string[] lines)
+        private static Node GetTree(string[] lines)
         {
-            var parent = GetParent(lines);
-            var leafs = lines.Select(t => GetNode(t)).ToDictionary(k => k.Name);
-            parent.FindChildren(leafs);
-            RecursiveCorrection(parent);
-            return CorrectNodeValue;
+            var ParentNodes = GetParentNodes(lines);
+            return ParentNodes.First(n => !ParentNodes
+            .Any(c => c.Value.SubProgramNameList
+            .Any(x => x.Equals(n.Value.Name)))).Value;
         }
 
-        private static Node GetParent(string[] lines)
+        public static int WeightToBeBalanced(string[] lines)
         {
-            if (ParentNodes == null || ParentNodes.Count == 0) 
-                ParentNodes = GetParentNodes(lines);
-
-            var firstParent = ParentNodes.First(n => !ParentNodes.Any(c => c.Value.SubProgramNameList.Any(x => x.Equals(n.Value.Name))));
-            return firstParent.Value;
+            var parent = GetTree(lines);
+            var leafs = lines.Select(t => GetNode(t)).ToDictionary(k => k.Name);
+            parent.FindChildren(leafs);
+            return RecursiveCorrection(parent);
         }
 
         private static Node GetNode(string node)
@@ -36,7 +32,7 @@
 
             return new Node(nodeName, weight, subp);
         }
-        private static void RecursiveCorrection(Node wrongNode, int goodvalue = 0)
+        private static int RecursiveCorrection(Node wrongNode, int goodvalue = 0, int correctNodeValue = -1)
         {
             var grouped = wrongNode.Children.GroupBy(x => x.FullNodeWeight);
             var goodNode = grouped.Where(g => g.Count() > 1).First().First();
@@ -47,22 +43,28 @@
 
                 if (goodvalue == 0) goodvalue = goodNode.FullNodeWeight - falseNode.FullNodeWeight;
 
-                CorrectNodeValue = falseNode.Weight + goodvalue;
-                if (falseNode.StillHaveChildren() && falseNode.Children.GroupBy(x => x.FullNodeWeight).Count() > 1) RecursiveCorrection(falseNode, goodvalue);
+                correctNodeValue = falseNode.Weight + goodvalue;
+                if (falseNode.StillHaveChildren() 
+                    && falseNode.Children.GroupBy(x => x.FullNodeWeight).Count() > 1) 
+                    return RecursiveCorrection(falseNode, goodvalue);
             }
+
+            return correctNodeValue;
         }
 
-        private static Dictionary<string, Node> ParentNodes = new();
-        private static Dictionary<string, Node> GetParentNodes(string[] lines) => lines.Where(n => n.Contains("->"))
-                                                                                       .Select(t => GetNode(t)).ToDictionary(x => x.Name);
+        private static Dictionary<string, Node> GetParentNodes(string[] lines) 
+            => lines.Where(n => n.Contains("->"))
+            .Select(t => GetNode(t)).ToDictionary(x => x.Name);
     }
 
     class Node
     {
         public List<Node> Children { get; set; } = new List<Node>();
         public Node Parent { get; set; }
+
         public string Name { get; set; }
         public int Weight { get; set; }
+
         public List<string> SubProgramNameList { get; set; } = new List<string>();
         public int ChildrenWeight { get => Children.Sum(x => x.FullNodeWeight); }
         public int FullNodeWeight { get => ChildrenWeight + Weight; }
@@ -74,7 +76,6 @@
             Weight = weight;
             SubProgramNameList = strProgList;
         }
-
         public void FindChildren(Dictionary<string, Node> allnodes)
         {
             foreach (string c in SubProgramNameList)
---
+++ Tests/2017/Day07.cs
@@ -0,0 +1,50 @@
+﻿using Days._2017;
+using Xunit;
+
+namespace Tests._2017
+{
+    public class Day07
+    {
+        private readonly static string[] test =
+        {
+            "pbga (66)",
+            "xhth (57)",
+            "ebii (61)",
+            "havc (66)",
+            "ktlj (57)",
+            "fwft (72) -> ktlj, cntj, xhth",
+            "qoyq (66)",
+            "padx (45) -> pbga, havc, qoyq",
+            "tknk (41) -> ugml, padx, fwft",
+            "jptl (61)",
+            "ugml (68) -> gyxo, ebii, jptl",
+            "gyxo (61)",
+            "cntj (57)"
+        };
+        [Fact]
+        public void FirstStarExample()
+        {
+            Assert.Equal("tknk", RecursiveCircus.GetNameBottom(test));
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2017\\07.txt");
+            Assert.Equal("hlhomy", RecursiveCircus.GetNameBottom(dataset));
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            Assert.Equal(60, RecursiveCircus.WeightToBeBalanced(test));
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2017\\07.txt");
+            Assert.Equal(1505, RecursiveCircus.WeightToBeBalanced(dataset));
+        }
+    }
+}
