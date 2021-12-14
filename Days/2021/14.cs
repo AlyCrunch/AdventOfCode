4916f57b2e60a5106e61e2557c2fe471e0f7cbc6
AlyCrunch <alison.jarosz@viacesi.fr>
2021-12-14 20:34:59 +0100

    Trying to solve Day 14

---
+++ Days/2021/14.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Days._2021
+{
+    public static class ExtendedPolymerization
+    {
+
+        public static long GetSubstractMostAndLeastCommon(string[] datas, int step)
+        {
+            var (template, pairs) = datas.Format();
+            var templateFormatted = template.ToDictionary(pairs);
+
+            var lastState = Enumerable.Range(0, step)
+                .Aggregate(templateFormatted, (template, _) => template.NexStep(pairs));
+            var test = lastState.Aggregate(new List<(string, int)>(), (dic, element)
+                 => dic.Append((pairs[element.Key], element.Value)).ToList())
+                                .GroupBy(x => x.Item1)
+                                .ToDictionary(x => x.Key, x => x.Sum(z => z.Item2));
+            //dic.Append(new KeyValuePair<string, int>(pairs[element.Key], element.Value)).ToList());
+
+
+            return test.Max(x => x.Value) - test.Min(x => x.Value);
+        }
+        public static Dictionary<string, int> NexStep(this Dictionary<string, int> keys, Dictionary<string, string> pairs)
+        {
+            var newPairs = new Dictionary<string, int>();
+            foreach (var (key, value) in keys)
+            {
+                var c = pairs[key];
+                newPairs.Add(key[0] + c, value);
+                newPairs.Add(c + key[1], value);
+            }
+
+            return newPairs;
+        }
+
+        private static Dictionary<string, int> ToDictionary(this string template, Dictionary<string, string> pairs)
+            => Enumerable.Range(0, template.Length - 1)
+            .Aggregate(Enumerable.Empty<(string, int)>(), (t, i) => t.Append(("" + template[i] + template[i + 1], 1)))
+            .ToDictionary(x => x.Item1, x => x.Item2);
+
+        private static (string start, Dictionary<string, string> pairs) Format(this string[] datas)
+            => (datas[0], datas.Skip(2).Select(x => x.Split(" -> ")).ToDictionary(x => x[0], x => x[1]));
+    }
+}
---
+++ Tests/2021/Day14.cs
@@ -0,0 +1,56 @@
+﻿using Days._2021;
+using Xunit;
+
+namespace Tests._2021
+{
+    public class Day14
+    {
+        private readonly string[] test = new string[]
+        {
+            "NNCB",
+            "",
+            "CH -> B",
+            "HH -> N",
+            "CB -> H",
+            "NH -> C",
+            "HB -> C",
+            "HC -> B",
+            "HN -> C",
+            "NN -> C",
+            "BH -> H",
+            "NC -> B",
+            "NB -> B",
+            "BN -> B",
+            "BB -> N",
+            "BC -> B",
+            "CC -> N",
+            "CN -> C"
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            Assert.Equal(1588, ExtendedPolymerization.GetSubstractMostAndLeastCommon(test, 10));
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2021\\14.txt");
+            Assert.Equal(3306, ExtendedPolymerization.GetSubstractMostAndLeastCommon(dataset, 10));
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            Assert.Equal(1588, ExtendedPolymerization.GetSubstractMostAndLeastCommon(test, 40));
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2021\\14.txt");
+            Assert.Equal(3306, ExtendedPolymerization.GetSubstractMostAndLeastCommon(dataset, 40));
+        }
+    }
+}
