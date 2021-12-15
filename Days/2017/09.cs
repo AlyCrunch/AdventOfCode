ae8ba90abf4614bc2c77c0b89d44a920bb813ed1
AlyCrunch <alison.jarosz@viacesi.fr>
2021-12-15 15:42:55 +0100

    [2017] solves day 9

--- Days/2017/09.cs
+++ Days/2017/09.cs
@@ -2,54 +2,25 @@
 {
     public static class StreamProcessing
     {
-        static public int GetTotalScore(string str)
+        static public (int score, int garbageCount) GetTotalScore(string str)
         {
             bool garbage = false;
             int score = 0;
             int depth = 1;
+            int garbageCount = 0;
 
             for (int i = 0; i < str.Length; i++)
             {
                 var c = str[i];
                 if (c == '!') i++;
-                else if (garbage && c != '>') continue;
+                else if (garbage && c != '>') garbageCount++;
                 else if (c == '<') garbage = true;
                 else if (c == '>') garbage = false;
                 else if (c == '{') score += depth++;
                 else if (c == '}') depth--;
             }
 
-            return score;
+            return (score, garbageCount);
         }
-
-        static public int FindAllCharactersInGarbage(string str)
-        {
-
-            int startAt = -1;
-            List<string> garbages = new();
-
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (i != 0) if (str[i - 1] == '!') continue;
-                if (str[i] == '<') startAt = i;
-                else if (str[i] == '>') garbages.Add(str[startAt..i]);
-            }
-
-            return garbages.Select(x => GetGarbageCharactersCount(x)).Sum();
-        }
-
-        static public int GetGarbageCharactersCount(string str)
-        {
-            int count = 0;
-            for (int i = 1; i < str.Length - 1; i++)
-            {
-                if (str[i] == '!') continue;
-                if (i - 1 >= 0 && str[i - 1] == '!') continue;
-                count++;
-            }
-            return count;
-        }
-        //=> Enumerable.Range(1, str.Length - 2).Aggregate(0, (sum, i)
-        //    => (str[i] != '!') ? (i - 1 >= 0 && str[i - 1] != '!') ? sum + 1 : sum : sum);
     }
 }
