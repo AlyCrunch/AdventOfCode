8895cb6ccb2d22ccf7a950ae26efc0363d711bc5
Alison <alison.jarosz@viacesi.fr>
2021-12-15 02:44:33 +0100

    [2017] Adds and Test Day 9 (part1)

--- Days/2017/09.cs
+++ Days/2017/09.cs
@@ -1,4 +1,4 @@
-﻿namespace Days
+﻿namespace Days._2017
 {
     public static class StreamProcessing
     {
@@ -21,5 +21,35 @@
 
             return score;
         }
+
+        static public int FindAllCharactersInGarbage(string str)
+        {
+
+            int startAt = -1;
+            List<string> garbages = new();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (i != 0) if (str[i - 1] == '!') continue;
+                if (str[i] == '<') startAt = i;
+                else if (str[i] == '>') garbages.Add(str[startAt..i]);
+            }
+
+            return garbages.Select(x => GetGarbageCharactersCount(x)).Sum();
+        }
+
+        static public int GetGarbageCharactersCount(string str)
+        {
+            int count = 0;
+            for (int i = 1; i < str.Length - 1; i++)
+            {
+                if (str[i] == '!') continue;
+                if (i - 1 >= 0 && str[i - 1] == '!') continue;
+                count++;
+            }
+            return count;
+        }
+        //=> Enumerable.Range(1, str.Length - 2).Aggregate(0, (sum, i)
+        //    => (str[i] != '!') ? (i - 1 >= 0 && str[i - 1] != '!') ? sum + 1 : sum : sum);
     }
 }
