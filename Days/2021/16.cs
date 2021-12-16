7e359d38c9480ca986b10aa709eeb76fe7247437
Alison <alison.jarosz@viacesi.fr>
2021-12-16 20:59:32 +0100

    Adds Day 16 files

---
+++ Days/2021/16.cs
@@ -0,0 +1,97 @@
+﻿namespace Days._2021
+{
+    public static class PacketDecoder
+    {
+        private static readonly Dictionary<char, string> hex2bin = new()
+        {
+            { '0', "0000" },
+            { '1', "0001" },
+            { '2', "0010" },
+            { '3', "0011" },
+            { '4', "0100" },
+            { '5', "0101" },
+            { '6', "0110" },
+            { '7', "0111" },
+            { '8', "1000" },
+            { '9', "1001" },
+            { 'A', "1010" },
+            { 'B', "1011" },
+            { 'C', "1100" },
+            { 'D', "1101" },
+            { 'E', "1110" },
+            { 'F', "1111" }
+        };
+        private static readonly Dictionary<string, char> bin2hex = new()
+        {
+            { "0000", '0' },
+            { "0001", '1' },
+            { "0010", '2' },
+            { "0011", '3' },
+            { "0100", '4' },
+            { "0101", '5' },
+            { "0110", '6' },
+            { "0111", '7' },
+            { "1000", '8' },
+            { "1001", '9' },
+            { "1010", 'A' },
+            { "1011", 'B' },
+            { "1100", 'C' },
+            { "1101", 'D' },
+            { "1110", 'E' },
+            { "1111", 'F' }
+        };
+        //3 bits = version //3 bits = packet type ID
+        // ID 4 = litteral value 
+        // ID != 4 => operator
+        //length type ID : 0 => next 15 bits => total length in bits sub packet
+        //length type ID : 1 => next 11 bits => nb sub-packets immediately contained
+        // after groups of 5 bits, start with 1 if not the last/ 0 if last
+        public static int GetSumVersions(string v)
+        {
+            var binary = HexToBinary(v);
+
+            throw new NotImplementedException();
+        }
+
+        public static (int version, int type, List<string> sub) GetPacket(string binary)
+        {
+            (int version, int type, List<string> sub) packet = new();
+            packet.version = bin2hex["0" + binary[0..2]] - '0';
+            packet.sub = new();
+            if (binary[3..5] == "100")
+            {
+                packet.type = 4;
+                int i = 0;
+                do
+                {
+                    packet.sub.Add(binary[(7 + i)..(10 + i)]);
+                    if (binary[6 + i] == '0') break;
+                    i += 5;
+                }
+                while (true);
+            }
+            else
+            {
+                if(binary[6] == '1')
+                {
+                    int nbSub = Convert.ToInt32(binary[7..17], 2);
+                    for (int i = 1; i <= nbSub; i++)
+                    {
+                        packet.sub.Add(binary[(7 + i * 11)..(17 + i * 11)]);
+                    }
+                }
+                else
+                {
+
+                }
+            }
+
+            return packet;
+        }
+
+
+        public static string HexToBinary(string str)
+            => str.Aggregate("", (bits, c)
+                => bits + hex2bin[c]);
+    }
+}
---
+++ Tests/2021/Day16.cs
@@ -0,0 +1,34 @@
+﻿using Days._2021;
+using Xunit;
+
+namespace Tests._2021
+{
+    public class Day16
+    {
+        [Fact]
+        public void FirstStarExample()
+        {
+            Assert.Equal("110100101111111000101000", PacketDecoder.HexToBinary("D2FE28"));
+            Assert.Equal(16, PacketDecoder.GetSumVersions("8A004A801A8002F478"));
+            Assert.Equal(12, PacketDecoder.GetSumVersions("620080001611562C8802118E34"));
+            Assert.Equal(23, PacketDecoder.GetSumVersions("C0015000016115A2E0802F182340"));
+            Assert.Equal(31, PacketDecoder.GetSumVersions("A0016C880162017C3686B18A3D4780"));
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            //var dataset = Days.Helpers.ReadFile("Inputs\\year\\day.txt");
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+        }
+    }
+}
