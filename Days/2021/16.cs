c60607009751900381bb22d00b07a56b07a5ee17
AlyCrunch <alison.jarosz@viacesi.fr>
2021-12-17 17:12:32 +0100

    solves part 1 day 16

--- Days/2021/16.cs
+++ Days/2021/16.cs
@@ -2,13 +2,12 @@
 {
     public static class PacketDecoder
     {
-        public static int GetSumVersions(string v)
+        public static long GetSumVersions(string v)
             => GetAllPackets(HexToBinary(v)).GetSumVersion;
         public static Packet GetAllPackets(Queue<char> bits)
         {
-            Packet packet = new();
-            packet.version = BinaryToInt(bits.DequeueChunk(3));
-            packet.type = BinaryToInt(bits.DequeueChunk(3));
+            Packet packet = new(bits.ToList(), BinaryToInt(bits.DequeueChunk(3)), BinaryToInt(bits.DequeueChunk(3)));
+            
             switch (packet.type)
             {
                 case 4:
@@ -29,9 +28,9 @@
                     {
                         for (int i = 0; i < packet.length; i++)
                         {
-                            packet.rest = bits.ToList();
-                            var subpacket = GetAllPackets(new Queue<char>(packet.rest));
+                            var subpacket = GetAllPackets(bits);
                             packet.subPackets.Add(subpacket);
+                            bits = new Queue<char>(subpacket.rest);
                             packet.rest = subpacket.rest.ToList();
                         }
                     }
@@ -45,40 +44,48 @@
                             subPackets = subpacket.rest.ToList();
                         }
                         while (subPackets.Any());
+                        packet.rest = bits.ToList();
                     }
                     break;
             }
             return packet;
         }
 
-        private static int BinaryToInt(IEnumerable<char> bits)
-            => Convert.ToInt32(string.Join("", bits).PadLeft(4, '0'), 2);
+        private static long BinaryToInt(IEnumerable<char> bits)
+            => Convert.ToInt64(string.Join("", bits).PadLeft(4, '0'), 2);
 
         public static Queue<char> HexToBinary(string str)
-            => new(str.Select(c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')).SelectMany(x => x));
+            => new(str.Select(c => Convert.ToString(Convert.ToInt64(c.ToString(), 16), 2).PadLeft(4, '0')).SelectMany(x => x));
             
         public class Packet
         {
-            public int version;
-            public int type;
+            public long version;
+            public long type;
             public bool isNumberOfSub;
-            public int length;
+            public long length;
             public List<Packet> subPackets = new();
             public List<char> value = new();
             public List<char> rest = new();
+            public List<char> source = new();
             public int GetLengthSize
                 => (isNumberOfSub) ? 11 : 15;
-            public int GetSumVersion
+            public long GetSumVersion
                 => version + subPackets.Sum(x => x.GetSumVersion);
-            public int GetNumber
+            public long GetNumber
                 => value.Any() ? BinaryToInt(value) : 0 + subPackets.Sum(x => x.GetNumber);
+            public Packet(List<char> source, long version, long type)
+            {
+                this.version = version;
+                this.type = type;
+                this.source = source;
+            }
         }
     }
     public static class QueueExtensions
     {
-        public static IEnumerable<T> DequeueChunk<T>(this Queue<T> queue, int chunkSize)
+        public static IEnumerable<T> DequeueChunk<T>(this Queue<T> queue, long chunkSize)
         {
-            for (int i = 0; i < chunkSize && queue.Count > 0; i++)
+            for (long i = 0; i < chunkSize && queue.Count > 0; i++)
             {
                 yield return queue.Dequeue();
             }
--- Tests/2021/Day16.cs
+++ Tests/2021/Day16.cs
@@ -10,8 +10,7 @@ namespace Tests._2021
         [Fact]
         public void FirstStarExample()
         {
-            //var packet = PacketDecoder.GetAllPackets(PacketDecoder.HexToBinary("620080001611562C8802118E34"));
-            //Assert.Equal(16, PacketDecoder.GetSumVersions("8A004A801A8002F478"));
+            Assert.Equal(16, PacketDecoder.GetSumVersions("8A004A801A8002F478"));
             Assert.Equal(12, PacketDecoder.GetSumVersions("620080001611562C8802118E34"));
             Assert.Equal(23, PacketDecoder.GetSumVersions("C0015000016115A2E0802F182340"));
             Assert.Equal(31, PacketDecoder.GetSumVersions("A0016C880162017C3686B18A3D4780"));
@@ -21,7 +20,7 @@ namespace Tests._2021
         public void FirstStarSolution()
         {
             var dataset = Days.Helpers.ReadSingleLineFile("Inputs\\2021\\16.txt");
-            Assert.Equal(9, PacketDecoder.GetSumVersions(dataset));
+            Assert.Equal(920, PacketDecoder.GetSumVersions(dataset));
         }
 
         [Fact]
