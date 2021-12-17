e6346566d6841c9c15185fac782b3c07842ff3cd
Alison <alison.jarosz@viacesi.fr>
2021-12-17 10:47:04 +0100

    day 16 start

--- Days/2021/16.cs
+++ Days/2021/16.cs
@@ -40,58 +40,94 @@
             { "1110", 'E' },
             { "1111", 'F' }
         };
-        //3 bits = version //3 bits = packet type ID
-        // ID 4 = litteral value 
-        // ID != 4 => operator
-        //length type ID : 0 => next 15 bits => total length in bits sub packet
-        //length type ID : 1 => next 11 bits => nb sub-packets immediately contained
-        // after groups of 5 bits, start with 1 if not the last/ 0 if last
+
         public static int GetSumVersions(string v)
+            => GetAllPackets(HexToBinary(v)).GetSumVersion;
+        //000000000111111111222222222233333333334444444444555
+        //01234567890123456789012345678901234567890123456789012
+        //VVVTTTAAAAABBBBBCCCCC                                       (4)
+        //VVVTTTILLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCC       1 (not 4)
+        //VVVTTTILLLLLLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBBBBBB         0 (not 4)
+        // V = Version
+        // T = Type 4 => read packet / type != 4 subpacket
+        // if type 4 => groups of 5 bits, start with 1 if !last / 0 if last
+        // if type != 4
+        // I = Length Type ID (0 = 15 bits=> length(L) of sum sub packet)
+        //                    (1 = 11 bits=> 11bits of subpackets n times (L))
+        // L = 15/11 bits represent the length or count 
+        // ABCD.. subpackets
+        public static Packet GetAllPackets(string binary)
         {
-            var binary = HexToBinary(v);
-
-            throw new NotImplementedException();
-        }
-
-        public static (int version, int type, List<string> sub) GetPacket(string binary)
-        {
-            (int version, int type, List<string> sub) packet = new();
-            packet.version = bin2hex["0" + binary[0..2]] - '0';
-            packet.sub = new();
-            if (binary[3..5] == "100")
+            Packet packet = new();
+            packet.source = binary;
+            packet.version = bin2hex[ToBits(binary[0..3])] - '0';
+            packet.type = bin2hex[ToBits(binary[3..6])] - '0';
+            switch (packet.type)
             {
-                packet.type = 4;
-                int i = 0;
-                do
-                {
-                    packet.sub.Add(binary[(7 + i)..(10 + i)]);
-                    if (binary[6 + i] == '0') break;
-                    i += 5;
-                }
-                while (true);
-            }
-            else
-            {
-                if(binary[6] == '1')
-                {
-                    int nbSub = Convert.ToInt32(binary[7..17], 2);
-                    for (int i = 1; i <= nbSub; i++)
+                case 4:
+                    foreach (var group in binary[6..].Select((c, i) => new { Key = i / 5, Value = c }).GroupBy(x => x.Key, x => x.Value))
                     {
-                        packet.sub.Add(binary[(7 + i * 11)..(17 + i * 11)]);
+                        packet.value += string.Join("", group)[1..];
+                        if (group.ElementAt(0) == '0')
+                            break;
                     }
-                }
-                else
-                {
+                    packet.rest = packet.source.Replace(packet.value, "");
+                    break;
+                default:
+                    packet.isContainingSubpackets = binary[6] == '1';
+                    try
+                    {
+                        packet.length = Convert.ToInt32(binary[7..(7 + packet.GetLengthSize)], 2);
+                    }
+                    catch
+                    {
+                        throw new Exception($"{binary}[7..{packet.GetLengthSize}]");
+                    }
 
-                }
+                    if (packet.isContainingSubpackets)
+                    {
+                        foreach (var group in binary[18..].Select((c, i) => new { Key = i / 11, Value = c })
+                            .GroupBy(x => x.Key, x => x.Value).Take(packet.length))
+                        {
+                            packet.SubPackets.Add(GetAllPackets(string.Join("", group)));
+                        }
+                    }
+                    else
+                    {
+                        string substring = binary[22..(22 + packet.length)];
+                        do
+                        {
+                            var subpacket = GetAllPackets(substring);
+                            packet.SubPackets.Add(subpacket);
+                            substring = subpacket.rest;
+                        }
+                        while (substring.Length >= packet.length);
+                    }
+                    break;
             }
-
             return packet;
         }
 
+        private static string ToBits(string str)
+            => str.PadLeft(4, '0');
 
         public static string HexToBinary(string str)
             => str.Aggregate("", (bits, c)
                 => bits + hex2bin[c]);
+        public class Packet
+        {
+            public int version;
+            public int type;
+            public bool isContainingSubpackets;
+            public int length;
+            public List<Packet> SubPackets = new();
+            public string value = string.Empty;
+            public string rest = string.Empty;
+            public string source = string.Empty;
+            public int GetLengthSize
+                => (isContainingSubpackets) ? 11 : 15;
+            public int GetSumVersion
+                => version + SubPackets.Sum(x => x.GetSumVersion);
+        }
     }
 }
--- Tests/2021/Day16.cs
+++ Tests/2021/Day16.cs
@@ -1,4 +1,6 @@
 ﻿using Days._2021;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Tests._2021
@@ -8,11 +10,19 @@ namespace Tests._2021
         [Fact]
         public void FirstStarExample()
         {
-            Assert.Equal("110100101111111000101000", PacketDecoder.HexToBinary("D2FE28"));
-            Assert.Equal(16, PacketDecoder.GetSumVersions("8A004A801A8002F478"));
-            Assert.Equal(12, PacketDecoder.GetSumVersions("620080001611562C8802118E34"));
-            Assert.Equal(23, PacketDecoder.GetSumVersions("C0015000016115A2E0802F182340"));
-            Assert.Equal(31, PacketDecoder.GetSumVersions("A0016C880162017C3686B18A3D4780"));
+            //Assert.Equal(20, PacketDecoder.GetSumVersions("38006F45291200"));
+            var packet = PacketDecoder.GetAllPackets(PacketDecoder.HexToBinary("38006F45291200"));
+            Assert.Equal(1, packet.version); // 6 - 2
+            Assert.Equal("110100010100101001000100100", packet.SubPackets.First().source);
+            //Assert.Equal("11010001010", packet.SubPackets.First().value);
+            //Assert.Equal("0101001000100100", packet.SubPackets.First().rest);
+            //0101001000100100
+            //1010101010000010
+
+            //Assert.Equal(12, PacketDecoder.GetSumVersions("8A004A801A8002F478"));
+            //Assert.Equal(12, PacketDecoder.GetSumVersions("620080001611562C8802118E34"));
+            //Assert.Equal(23, PacketDecoder.GetSumVersions("C0015000016115A2E0802F182340"));
+            //Assert.Equal(31, PacketDecoder.GetSumVersions("A0016C880162017C3686B18A3D4780"));
         }
 
         [Fact]
