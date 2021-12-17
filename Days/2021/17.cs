c8be6cf76843b024ce78056eec62069f46bc72e0
AlyCrunch <alison.jarosz@viacesi.fr>
2021-12-17 20:19:48 +0100

    2021 adds file for day 17

---
+++ Days/2021/17.cs
@@ -0,0 +1,33 @@
+﻿namespace Days._2021
+{
+    public static class TrickShot
+    {
+
+
+        public static int GetTheHighestY(string input)
+        {
+            var target = FormatTarget(input);
+            Dictionary<(int x, int y), int> MaxYValues = new();
+            return 0;
+        }
+
+        public static ((int min, int max) x, (int min, int max) y) FormatTarget(string line)
+        {
+            var arr = line.Replace("target area: x=", "").Split(", y=")
+            .Select(x => x.Split("..")).ToArray();
+            return ((int.Parse(arr[0][0]), int.Parse(arr[0][1])), (int.Parse(arr[1][0]), int.Parse(arr[1][1])));
+        }
+
+        private static ((int x, int y) probe, (int x, int y) velocity) NextStep((int x, int y) probe, (int x, int y) velocity, ((int min, int max) x, (int min, int max) y) target)
+        {
+            probe.Add(velocity);
+            velocity.x = (velocity.x > 0) ? velocity.x - 1 : (velocity.x < 1) ? velocity.x + 1 : velocity.x;
+            velocity.y--;
+
+            return (probe, velocity);
+        }
+        private static (int x, int y) Add(this (int x, int y) a, (int x, int y) b)
+        => (a.x + b.x, a.y + b.y);
+
+    }
+}
---
+++ Tests/2021/Day17.cs
@@ -0,0 +1,32 @@
+﻿using Days._2021;
+using Xunit;
+
+namespace Tests._2021
+{
+    public class Day17
+    {
+        private static readonly string test = "target area: x=20..30, y=-10..-5";
+        private static readonly string dataset = "target area: x=277..318, y=-92..-53";
+
+        [Fact]
+        public void FirstStarExample()
+        {
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            //var dataset = Days.Helpers.ReadFile("Inputs\\year\\day.txt");
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+        }
+    }
+}
