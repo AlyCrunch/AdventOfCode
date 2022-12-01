d01fa500552560bd7f303894e501f7ba27e3b91c
Alison Jarosz <103586732+AlisonDotio@users.noreply.github.com>
2022-12-01 18:10:01 +0100

    Solves Day 1

---
+++ Days/2022/01.cs
@@ -0,0 +1,18 @@
+﻿namespace Days._2022
+{
+    public static class CalorieCounting
+    {
+        public static IEnumerable<int> GetListSumCalories(IEnumerable<string> calories)
+            => calories.Append(string.Empty).Aggregate((new List<int>(), 0), (acc, s)
+                 => string.IsNullOrEmpty(s) ?
+                     (acc.Item1.Append(acc.Item2).ToList(), 0)
+                 : (acc.Item1, acc.Item2 + int.Parse(s))).Item1;
+
+        public static int GetMaxCaloriesHeld(IEnumerable<string> calories)
+            => GetListSumCalories(calories).Max();
+
+
+        public static int GetTop3MaxHeld(IEnumerable<string> calories)
+            => GetListSumCalories(calories).OrderByDescending(x => x).Take(3).Sum();
+    }
+}
---
+++ Tests/2022/Day01.cs
@@ -0,0 +1,56 @@
+﻿using Days._2022;
+using Xunit;
+
+namespace Tests._2022
+{
+    public class Day01
+    {
+        private static readonly string[] test =
+               {
+            "1000",
+            "2000",
+            "3000",
+            "",
+            "4000",
+            "",
+            "5000",
+            "6000",
+            "",
+            "7000",
+            "8000",
+            "9000",
+            "",
+            "10000"};
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var result = CalorieCounting.GetMaxCaloriesHeld(test);
+            Assert.Equal(24000, result);
+
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\01.txt");
+            var result = CalorieCounting.GetMaxCaloriesHeld(dataset);
+            Assert.Equal(67658, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var result = CalorieCounting.GetTop3MaxHeld(test);
+            Assert.Equal(45000, result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\01.txt");
+            var result = CalorieCounting.GetTop3MaxHeld(dataset);
+            Assert.Equal(200158, result);
+        }
+    }
+}
