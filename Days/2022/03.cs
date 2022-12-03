543d87ca12ae1a966b0a5e1436b17ad9ea55a07a
Alison Jarosz <alison.jarosz@viacesi.fr>
2022-12-03 23:49:44 +0100

    Solves Day 3

---
+++ Days/2022/03.cs
@@ -0,0 +1,28 @@
+﻿namespace Days._2022
+{
+    public static class RucksackReorganization
+    {
+        public static (string, string) SplitInHalf(string str)
+            => (str[..(str.Length / 2)], str[(str.Length / 2)..]);
+
+        public static char GetItem((string first, string second) compartment)
+            => compartment.first.Intersect(compartment.second).First();
+
+        public static int GetPriority(this char c)
+            => (char.IsLower(c)) ? c - 96 : c - 38;
+
+        public static int GetSumPriorities(IEnumerable<string> input)
+            => input.Select(x => GetItem(SplitInHalf(x)).GetPriority()).Sum();
+
+        public static char GetItem(IEnumerable<string> group)
+            => group
+                .Skip(1)
+                .Aggregate(
+                    new HashSet<char>(group.First()),
+                    (h, e) => { h.IntersectWith(e); return h; }
+                ).First();
+
+        public static int GetGroupPriorities(IEnumerable<string> input)
+            => input.Chunk(3).Select(x => GetItem(x).GetPriority()).Sum();
+    }
+}
---
+++ Tests/2022/Day03.cs
@@ -0,0 +1,50 @@
+﻿using Days._2022;
+using System.Linq;
+using Xunit;
+
+namespace Tests._2022
+{
+    public class Day03
+    {
+        public readonly string[] test = {
+            "vJrwpWtwJgWrhcsFMMfFFhFp",
+            "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+            "PmmdzqPrVvPwwTWBwg",
+            "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+            "ttgJtRGJQctTZtZT",
+            "CrZsJsPPZsGzwwsLwLmpwMDw"};
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var result = RucksackReorganization.GetSumPriorities(test);
+            Assert.Equal(157, result);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\03.txt");
+
+            var result = RucksackReorganization.GetSumPriorities(dataset);
+            Assert.Equal(8153, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var result = RucksackReorganization.GetGroupPriorities(test);
+
+            Assert.Equal(70, result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\03.txt");
+            var result = RucksackReorganization.GetGroupPriorities(dataset);
+
+            Assert.Equal(2342, result);
+        }
+    }
+}
