a14e1bb5cb854337b608736bd6ae1f103d666a42
Alison Jarosz <alison.jarosz@viacesi.fr>
2022-12-04 17:05:10 +0100

    Solves Day 4

---
+++ Days/2022/04.cs
@@ -0,0 +1,23 @@
+﻿namespace Days._2022
+{
+    public static class CampCleanup
+    {
+        public static int CountContained(IEnumerable<string> input)
+            => input.Count(x => IsContained(GetRanges(x).ToArray()));
+
+        public static IEnumerable<int> GetRanges(string line)
+            => line.Split(',', '-').Select(x => int.Parse(x));
+
+        public static bool IsContained(int[] r)
+            => (r[0] >= r[2] && r[1] <= r[3]) ||
+               (r[2] >= r[0] && r[3] <= r[1]);
+
+
+        public static int CountOverlap(IEnumerable<string> input)
+            => input.Count(x => IsOverlapping(GetRanges(x).ToArray()));
+
+        public static bool IsOverlapping(int[] r)
+            => !(r[0] > r[3] || r[1] < r[2]);
+
+    }
+}
---
+++ Tests/2022/Day04.cs
@@ -0,0 +1,47 @@
+﻿using Days._2022;
+using Xunit;
+
+namespace Tests._2022
+{
+    public class Day04
+    {
+        private readonly string[] test = {
+            "2-4,6-8",
+            "2-3,4-5",
+            "5-7,7-9",
+            "2-8,3-7",
+            "6-6,4-6",
+            "2-6,4-8" 
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var result = CampCleanup.CountContained(test);
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void FirstStarSolution() 
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\04.txt");
+            var result = CampCleanup.CountContained(dataset);
+            Assert.Equal(462, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var result = CampCleanup.CountOverlap(test);
+            Assert.Equal(4, result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\04.txt");
+            var result = CampCleanup.CountOverlap(dataset);
+            Assert.Equal(835, result);
+        }
+    }
+}
