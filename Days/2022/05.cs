8c5f6b8b3466dc257a45b4b219392572661389f5
Alison Jarosz <alison.jarosz@viacesi.fr>
2022-12-06 13:52:54 +0100

    Solves Day 5

---
+++ Days/2022/05.cs
@@ -0,0 +1,49 @@
+﻿namespace Days._2022
+{
+    public static class SupplyStacks
+    {
+        public static string ExecuteProcedure(IEnumerable<string> input, bool stackable = false)
+        {
+            var (Init, Procedure) = Format(input);
+            var end = Procedure.Aggregate(Init, (stack, p) => (stackable)?
+            ProceedMoveStack(stack, p): Proceed(stack, p)).Select(x => x.Peek());
+
+            return string.Join("", end);
+        }
+
+        public static (Stack<char>[] Init, IEnumerable<int[]> Procedure) Format(IEnumerable<string> input)
+        {
+            var space = input.ToList().IndexOf("");
+            var init = GetInitCrates(input.Take(space - 1));
+            var delimiters = new string[] { "move "," from ", " to" };
+            var procedures = input.Skip(space + 1)
+                .Select(x => x.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Select(y => int.Parse(y)).ToArray());
+            return (init, procedures);
+        }
+
+        public static Stack<char>[] GetInitCrates(IEnumerable<string> input)
+        {
+            var index = Enumerable.Range(0, input.Last().Length -1 )
+                .Aggregate(new List<int>(), (indexes, x) => { 
+                    if (char.IsLetter(input.Last()[x])) indexes.Add(x); return indexes; });
+
+            return index.Aggregate(new List<Stack<char>>(), (stacks, i) =>
+                {
+                    stacks.Add(
+                      Enumerable.Range(0, input.Count()).Reverse()
+                      .Aggregate(new Stack<char>(), (stack, x) =>
+                      { if (char.IsLetter(input.ElementAt(x)[i])) stack.Push(input.ElementAt(x)[i]); return stack; })
+                      ); return stacks;
+                }).ToArray();
+        }
+
+        public static Stack<char>[] Proceed(Stack<char>[] cranes, int[] p)
+            => Enumerable.Range(0, p[0])
+            .Aggregate(cranes, (c, x) => { cranes[p[2] - 1].Push(cranes[p[1] - 1].Pop()); return cranes; });
+
+        public static Stack<char>[] ProceedMoveStack(Stack<char>[] cranes, int[] p)
+        {
+            return cranes[p[1] - 1].ToList().Take(p[0]).Reverse().Aggregate(cranes, (c, x) => { c[p[2] -1].Push(x); c[p[1] -1].Pop(); return c; });
+        }
+    }
+}
---
+++ Tests/2022/Day05.cs
@@ -0,0 +1,51 @@
+﻿using Days._2022;
+using Xunit;
+
+namespace Tests._2022
+{
+    public class Day05
+    {
+        private static readonly string[] test =
+        {
+            "    [D]    ",
+            "[N] [C]    ",
+            "[Z] [M] [P]",
+            " 1   2   3 ",
+            "",
+            "move 1 from 2 to 1",
+            "move 3 from 1 to 3",
+            "move 2 from 2 to 1",
+            "move 1 from 1 to 2"
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var result = SupplyStacks.ExecuteProcedure(test);
+            Assert.Equal("CMZ", result);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\05.txt");
+            var result = SupplyStacks.ExecuteProcedure(dataset);
+            Assert.Equal("RTGWZTHLD", result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var result = SupplyStacks.ExecuteProcedure(test, true);
+            Assert.Equal("MCD", result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\05.txt");
+            var result = SupplyStacks.ExecuteProcedure(dataset, true);
+            Assert.Equal("STHGRZZFR", result);
+        }
+    }
+}
