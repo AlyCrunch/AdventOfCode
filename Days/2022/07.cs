55520884d126083608b1b33bfca2d5f4e18496ca
Alison Jarosz <alison.jarosz@viacesi.fr>
2022-12-11 11:17:48 +0100

    Solves Day 7

---
+++ Days/2022/07.cs
@@ -0,0 +1,81 @@
+﻿namespace Days._2022
+{
+    public class NoSpaceLeftOnDevice
+    {
+        private Directory GetRoot(Directory dir)
+            => (dir.Name == "/") ? dir : GetRoot(dir.Ancestor);    
+
+        public static Directory Format(string cmd, Directory curr)
+        {
+            var param = cmd.Split(" ");
+            if (param[0] == "$")
+            {
+                if (param[1] == "ls") return curr;
+                if (param[1] == "cd")
+                {
+                    if (param[2] == "..")
+                        return curr.Ancestor;
+                    else
+                        return curr.Children.First(x => x.Name == param[2]);
+                }
+            }
+            else
+            {
+                if (param[0] == "dir")
+                {
+                    curr.Children.Add(new Directory(param[1], curr));
+                    return curr;
+                }
+                else
+                {
+                    curr.Files.Add((long.Parse(param[0]), param[1]));
+                    return curr;
+                }
+            }
+
+            throw new Exception("oopsie");
+        }
+
+        public long GetTotalSumDirectories(string[] input)
+        {
+            var formatted = input.Skip(1).Aggregate(new Directory("/"), (dir, i) => Format(i, dir));
+            var root = GetRoot(formatted);
+            var flat = root.Flatten();
+
+            return flat.Sum(x => x.SizeHundredK()) + root.SizeHundredK();
+        }
+
+        public long GetDirectoryToDelete(string[] input)
+        {
+            var formatted = input.Skip(1).Aggregate(new Directory("/"), (dir, i) => Format(i, dir));
+            var root = GetRoot(formatted);
+            var maxSize = root.Size();
+            var flat = root.Flatten();
+            var bigEnough = flat.Where(x => x.IsBigEnough(30000000 - (70000000 - maxSize)));
+
+            return bigEnough.Min(x => x.Size());
+        }
+    }
+
+    public class Directory
+    {
+        public string Name { get; set; } = "";
+        public Directory Ancestor { get; set; }
+        public List<Directory> Children { get; set; } = new();
+        public HashSet<(long, string)> Files { get; set; } = new();
+
+        public Directory(string name) => Name = name;
+        public Directory(string name, Directory ancestor) { Name = name; Ancestor = ancestor; }
+
+        public IEnumerable<Directory> Flatten()
+            => Children.SelectMany(c => c.Flatten()).Concat(Children);
+
+        public bool IsBigEnough(long need)
+            => need <= Size();
+
+        public long Size()
+            => Files.Sum(x => x.Item1) + Children.Sum(x => x.Size());
+        public long SizeHundredK()
+            => (Size() > 100000) ? 0 : Size();
+    }
+}
---
+++ Tests/2022/Day07.cs
@@ -0,0 +1,63 @@
+﻿using Days._2022;
+using Xunit;
+
+namespace Tests._2022
+{
+    public class Day07
+    {
+        private static readonly string[] test = { 
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k" };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var result = new NoSpaceLeftOnDevice().GetTotalSumDirectories(test);
+            Assert.Equal(95437, result);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\07.txt");
+            var result = new NoSpaceLeftOnDevice().GetTotalSumDirectories(dataset);
+            Assert.Equal(919137, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var result = new NoSpaceLeftOnDevice().GetDirectoryToDelete(test);
+            Assert.Equal(24933642, result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\07.txt");
+            var result = new NoSpaceLeftOnDevice().GetDirectoryToDelete(dataset);
+            Assert.Equal(2877389, result);
+        }
+    }
+}
