26a99ada97bdee32e6a13e4e2bf97ccc1f75be52
Alison Jarosz <alison.jarosz@viacesi.fr>
2022-12-11 17:15:37 +0100

    Solves Day 8

---
+++ Days/2022/08.cs
@@ -0,0 +1,57 @@
+﻿namespace Days._2022
+{
+    public static class Treetop
+    {
+        private static int[][] Format(this string[] input)
+            => Enumerable.Range(0, input.Length)
+            .Aggregate(new List<int[]>(), (map, i) =>
+                { map.Add(input[i].Select(x => x - '0').ToArray()); return map; }).ToArray();
+
+        private static bool IsVisible(int x, int y, int[][] map)
+        {
+            if (x == 0 || y == 0 || x == map.Length - 1 || y == map.Length - 1) return true;
+            return GetAllSights(x, y, map).Any(m => map[x][y] > m.Max());
+        }
+
+        private static IEnumerable<IEnumerable<int>> GetAllSights(int x, int y, int[][] map)
+        {
+            var ToTop = Enumerable.Range(0, x).Select(i => map[i][y]).Reverse();
+            var ToBottom = Enumerable.Range(x + 1, map.Length - (x + 1)).Select(i => map[i][y]);
+            var ToLeft = Enumerable.Range(0, y).Select(i => map[x][i]).Reverse();
+            var ToRight = Enumerable.Range(y + 1, map.Length - (y + 1)).Select(i => map[x][i]);
+
+            return new List<IEnumerable<int>>() { ToTop, ToBottom, ToLeft, ToRight };
+        }
+
+        private static int GetScenicScore(int x, int y, int[][] map)
+        {
+            if (x == 0 || y == 0 || x == map.Length - 1 || y == map.Length - 1) return 0;
+            return GetAllSights(x, y, map).Aggregate(1, (s, sight) => s * GetNbOfTreeInSight(sight, map[x][y]));
+        }
+
+        private static int GetNbOfTreeInSight(IEnumerable<int> sight, int height)
+        {
+            var element = sight.Select((x, i) => new { Index = i, Height = x }).FirstOrDefault(x => height <= x.Height);
+            if (element == null) return sight.Count();
+            return element.Index + 1;
+        }
+
+        public static int CountTreesVisible(string[] test)
+        {
+            var formatted = Format(test);
+
+            return Enumerable.Range(0, test.Length).Aggregate(0, (sum, x)
+                => sum + Enumerable.Range(0, test.Length).Count(y => IsVisible(x, y, formatted)));
+        }
+
+        public static int GetHighestScenicScore(string[] test)
+        {
+            var formatted = Format(test);
+
+            return Enumerable.Range(0, test.Length).Aggregate(new List<int>(), (lines, x)
+                => { lines.Add(
+                    Enumerable.Range(0, test.Length).Max(y => GetScenicScore(x, y, formatted))); return lines; })
+                                                    .Max();
+        }
+    }
+}
---
+++ Tests/2022/Day08.cs
@@ -0,0 +1,45 @@
+﻿using Days._2022;
+using Xunit;
+
+namespace Tests._2022
+{
+    public class Day08
+    {
+        private static readonly string[] test = {
+            "30373",
+            "25512",
+            "65332",
+            "33549",
+            "35390"};
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            int result = Treetop.CountTreesVisible(test);
+            Assert.Equal(21, result);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\08.txt");
+            int result = Treetop.CountTreesVisible(dataset);
+            Assert.Equal(1807, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            int result = Treetop.GetHighestScenicScore(test);
+            Assert.Equal(8, result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2022\\08.txt");
+            int result = Treetop.GetHighestScenicScore(dataset);
+            Assert.Equal(480000, result);
+        }
+    }
+}
