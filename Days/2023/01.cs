6c6ebfe26d73dc3f4171fdd41c95389e7d0498e0
Alison Jarosz <alison.jarosz@viacesi.fr>
2023-12-01 19:41:40 +0100

    Solves day 1 2023

---
+++ Days/2023/01.cs
@@ -0,0 +1,50 @@
+﻿namespace Days._2023
+{
+    public class Trebuchet
+    {
+        private static readonly Dictionary<string, string> LiteralDigits = new()
+        {
+            ["zero"] = "0",
+            ["one"] = "1",
+            ["two"] = "2",
+            ["three"] = "3",
+            ["four"] = "4",
+            ["five"] = "5",
+            ["six"] = "6",
+            ["seven"] = "7",
+            ["eight"] = "8",
+            ["nine"] = "9",
+            ["0"] = "0",
+            ["1"] = "1",
+            ["2"] = "2",
+            ["3"] = "3",
+            ["4"] = "4",
+            ["5"] = "5",
+            ["6"] = "6",
+            ["7"] = "7",
+            ["8"] = "8",
+            ["9"] = "9",
+        };
+
+        public static int GetFirstLastDigitSum(IEnumerable<string> input)
+            => input.Aggregate(0, (sum, row) =>
+            sum + int.Parse(row.First(c => char.IsDigit(c)).ToString() + row.Last(c => char.IsDigit(c))));
+
+        public static int GetFirstLastDigitLiteralSum(IEnumerable<string> input)
+        {
+            var keywords = LiteralDigits.Keys.ToList();
+            return input.Aggregate(0, (sum, row) =>  
+                {
+                    var indexes = GetKeywordsIndexes(keywords, row);
+                    return sum + int.Parse(LiteralDigits[indexes.MinBy(x => x.index).key] + LiteralDigits[indexes.MaxBy(x => x.index).key]);
+                }            
+            );
+        }
+
+        private static IEnumerable<(int index, string key)> GetKeywordsIndexes(IEnumerable<string> keywords, string row)
+            => keywords.Select(key => new List<(int index, string key)>(){
+                (row.IndexOf(key), key),
+                (row.LastIndexOf(key), key)
+            }).SelectMany(i => i).Where(x => x.index != -1).Distinct();
+    }
+}
---
+++ Tests/2023/Day01.cs
@@ -0,0 +1,56 @@
+﻿using Days._2023;
+using Xunit;
+
+namespace Tests._2023
+{
+    public class Day01
+    {
+        private static readonly string[] test =
+               {
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet"};
+
+        private static readonly string[] test2 =
+               {
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen"};
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var result = Trebuchet.GetFirstLastDigitSum(test);
+            Assert.Equal(142, result);
+
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2023\\01.txt");
+            var result = Trebuchet.GetFirstLastDigitSum(dataset);
+            Assert.Equal(56108, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var result = Trebuchet.GetFirstLastDigitLiteralSum(test2);
+            Assert.Equal(281, result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2023\\01.txt");
+            var result = Trebuchet.GetFirstLastDigitLiteralSum(dataset);
+            Assert.Equal(55652, result);
+        }
+    }
+}
