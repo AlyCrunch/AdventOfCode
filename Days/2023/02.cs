1f127244ea37d65606dd12dbf2e3171a573149e3
Alison Jarosz <alison.jarosz@viacesi.fr>
2023-12-05 20:01:48 +0100

    Solves Day 2

---
+++ Days/2023/02.cs
@@ -0,0 +1,59 @@
+﻿namespace Days._2023
+{
+    public static class CubeConundrum
+    {
+
+
+        //            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        public static int GetSumPossible(IEnumerable<string> input, int blue, int red, int green)
+        {
+            string[] delimiters = { "; ", ", " };
+            var sum = 0;
+            for (int i = 1; i <= input.Count(); i++)
+            {
+                var impossible = input.ElementAt(i - 1).Split(": ")[1].Split(delimiters, StringSplitOptions.None)
+                    .Any(y =>
+                    {
+                        var param = y.Split(" ");
+                        return param[1] switch
+                        {
+                            "blue" => int.Parse(param[0]) > blue,
+                            "red" => int.Parse(param[0]) > red,
+                            "green" => int.Parse(param[0]) > green,
+                            _ => true
+                        };
+                    });
+                if (!impossible) sum += i;
+            }
+            return sum;
+        }
+
+        public static int GetMinimumSetMultiplied(IEnumerable<string> input)
+            => input.GetGame().Sum(x => x.Max(y => y.blue) * x.Max(y => y.red) * x.Max(y => y.green));
+
+
+        private static IEnumerable<IEnumerable<(int blue, int red, int green)>> GetGame(this IEnumerable<string> games)
+        {
+            foreach (var game in games)
+            {
+                yield return game.Split(": ")[1].Split("; ").Select(x => x.GetDraw());
+            }
+        }
+
+        private static (int blue, int red, int green) GetDraw(this string draw)
+        {
+            int r, g, b;
+            r = g = b = 0;
+
+            foreach (var ball in draw.Split(", "))
+            {
+                var param = ball.Split(" ");
+                if (param[1] == "blue") b = int.Parse(param[0]);
+                if (param[1] == "red") r = int.Parse(param[0]);
+                if (param[1] == "green") g = int.Parse(param[0]);
+            }
+            return (b, r, g);
+        }
+
+    }
+}
---
+++ Tests/2023/Day02.cs
@@ -0,0 +1,48 @@
+﻿using Days._2023;
+using Xunit;
+
+namespace Tests._2023
+{
+    public class Day02
+    {
+        private static readonly string[] test =
+        {
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+            "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+            "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var result = CubeConundrum.GetSumPossible(test, 14, 12, 13);
+            Assert.Equal(8, result);
+
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2023\\02.txt");
+            var result = CubeConundrum.GetSumPossible(dataset, 14, 12, 13);
+            Assert.Equal(2716, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var result = CubeConundrum.GetMinimumSetMultiplied(test);
+            Assert.Equal(2286, result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2023\\02.txt");
+            var result = CubeConundrum.GetMinimumSetMultiplied(dataset);
+            Assert.Equal(2286, result);
+        }
+    }
+}
