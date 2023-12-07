38c9934d1c3ce75650f472434f3bb62e8e3a7d96
Alison Jarosz <alison.jarosz@viacesi.fr>
2023-12-07 01:50:50 +0100

    Solves Day 3

---
+++ Days/2023/03.cs
@@ -0,0 +1,102 @@
+﻿namespace Days._2023
+{
+    public static class GearRatios
+    {
+        private static readonly List<(int, int)> _adjacents = new()
+        {
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        };
+
+        public static int SumPartNumbers(IEnumerable<string> input)
+        {
+            List<int> PartNumbers = new();
+            string temp = string.Empty;
+            bool isTempPart = false;
+
+            for (int line = 0; line < input.Count(); line++)
+            {
+                for (int col = 0; col < input.ElementAt(line).Length; col++)
+                {
+                    if (!char.IsDigit(input.ElementAt(line)[col])) continue;
+                    temp += input.ElementAt(line)[col];
+
+                    isTempPart = isTempPart || IsPartNumber(line, col, input);
+
+                    if (!char.IsDigit(input.Get(line, col + 1)))
+                    {
+                        if (isTempPart)
+                        {
+                            PartNumbers.Add(int.Parse(temp));
+                            isTempPart = false;
+                        }
+                        temp = string.Empty;
+                    }
+
+                }
+            }
+            return PartNumbers.Sum();
+        }
+
+        private static char Get(this IEnumerable<string> map, int row, int col)
+            => map.ElementAtOrDefault(row)?.ElementAtOrDefault(col) ?? '.';
+
+        private static bool IsSymbolExceptDot(this char c)
+            => c != '.' && c != '\0' && !char.IsDigit(c);
+
+        private static bool IsPartNumber(int row, int col, IEnumerable<string> input)
+            => _adjacents.Any(cords => input.Get(row + cords.Item1, col + cords.Item2).IsSymbolExceptDot());
+
+        public static int GetGearRatioSum(IEnumerable<string> input)
+        {
+            var (numbers, gears) = Format(input);
+            return gears.Aggregate(0, (sum, cords) => 
+            {
+                var adj = GetAdjacentsNumber(numbers, cords);
+                if (adj.Count() == 2)
+                    return sum + int.Parse(adj.ElementAt(0)) * int.Parse(adj.ElementAt(1));
+                return sum;
+            });
+        }
+
+        private static IEnumerable<string> GetAdjacentsNumber(List<(string num, (int row, int col))> numbers,(int row, int col) coord)
+        => _adjacents.Select(adjcoord => numbers.Where(x => x.Item2.row == coord.row + adjcoord.Item1
+                                   && coord.col + adjcoord.Item2 >= x.Item2.col 
+                                   && coord.col + adjcoord.Item2 < x.Item2.col + x.num.Length)
+                                .Select(x => x.num)).SelectMany(x => x).Distinct();
+
+        private static (List<(string num, (int row, int col))>, List<(int row, int col)>) Format(IEnumerable<string> input)
+        {
+            string temp = string.Empty;
+            List<(string num, (int row, int col))> numbers = new();
+            List<(int row, int col)> gears = new();
+
+            for (int line = 0; line < input.Count(); line++)
+            {
+                for (int col = 0; col < input.ElementAt(line).Length; col++)
+                {
+                    if (input.ElementAt(line)[col] == '*') gears.Add((line, col));
+                    if (!char.IsDigit(input.ElementAt(line)[col])) continue;
+
+                    if (!char.IsDigit(input.ElementAt(line)[col])) continue;
+                    temp += input.ElementAt(line)[col];
+
+                    if (!char.IsDigit(input.Get(line, col + 1)))
+                    {
+                        numbers.Add((temp, (line,col - (temp.Length -1))));
+                        temp = string.Empty;
+                    }
+                }
+            }
+
+            return (numbers, gears);
+        }
+
+    }
+}
---
+++ Tests/2023/Day03.cs
@@ -0,0 +1,52 @@
+﻿using Days._2023;
+using Xunit;
+
+namespace Tests._2023
+{
+    public class Day03
+    {
+        private static readonly string[] test =
+        {
+            "467..114..",
+            "...*......",
+            "..35..633.",
+            "......#...",
+            "617*......",
+            ".....+.58.",
+            "..592.....",
+            "......755.",
+            "...$.*....",
+            ".664.598.."
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var result = GearRatios.SumPartNumbers(test);
+            Assert.Equal(4361, result);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2023\\03.txt");
+            var result = GearRatios.SumPartNumbers(dataset);
+            Assert.Equal(520019, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var result = GearRatios.GetGearRatioSum(test);
+            Assert.Equal(467835, result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2023\\03.txt");
+            var result = GearRatios.GetGearRatioSum(dataset);
+            Assert.Equal(75519888, result);
+        }
+    }
+}
