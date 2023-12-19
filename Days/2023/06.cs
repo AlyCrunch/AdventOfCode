4895f13af7118228dd6f2b1a6d6184aa1381252f
Alison Jarosz <alison.jarosz@viacesi.fr>
2023-12-20 00:23:15 +0100

    Solves Day 6

---
+++ Days/2023/06.cs
@@ -0,0 +1,51 @@
+﻿namespace Days._2023
+{
+    public static class WaitForIt
+    {
+        private static List<(int time, int distance)> Format(string[] input)
+        {
+            var parse = input.Select(x => x.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                               .Skip(1)
+                               .Select(y => int.Parse(y)));
+
+            return parse.ElementAt(0).Zip(parse.ElementAt(1), (x, y) => (x, y)).ToList();
+        }
+
+        private static (long time, long distance) FormatLong(string[] input)
+        {
+            var parse = input.Select(x => 
+            long.Parse(x.Split(":", StringSplitOptions.RemoveEmptyEntries)[1].Replace(" ", "")));
+
+            return (parse.ElementAt(0), parse.ElementAt(1));
+
+        }
+
+        public static int GetProductRecords(string[] input)
+        {
+            var records = Format(input);
+            
+            return records.Select(x => Enumerable.Range(0, x.time + 1)
+                                                     .Select(y => DistanceByMs(y, x.time))
+                                                     .Count(y => y > x.distance))
+                               .Aggregate(1, (product, item) => product * item);
+        }
+
+        public static object GetConcatRecord(string[] input)
+        {
+            var (time, distance) = FormatLong(input);
+            long i;
+            for (i = 0; i < time; i++)
+            {
+                if (DistanceByMs(i, time) > distance)
+                    break;
+            }
+
+            return time + 1 - (i) * 2;
+        }
+
+        private static int DistanceByMs(int push, int time)
+            => (time - push) * push;
+        private static long DistanceByMs(long push, long time)
+            => (time - push) * push;
+    }
+}
---
+++ Tests/2023/Day06.cs
@@ -0,0 +1,48 @@
+﻿using Days._2023;
+using Xunit;
+
+namespace Tests._2023
+{
+    public class Day06
+    {
+        private static readonly string[] test =
+        {
+            "Time:      7  15   30",
+            "Distance:  9  40  200"
+        };
+
+        private static readonly string[] dataset =
+        {
+            "Time:        49     97     94     94",
+            "Distance:   263   1532   1378   1851"
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            var result = WaitForIt.GetProductRecords(test);
+            Assert.Equal(288, result);
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var result = WaitForIt.GetProductRecords(dataset);
+            Assert.Equal(4403592, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            var result = WaitForIt.GetConcatRecord(test);
+            Assert.Equal((long)71503, result);
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var result = WaitForIt.GetConcatRecord(dataset);
+            Assert.Equal((long)38017587, result);
+        }
+    }
+}
