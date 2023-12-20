99e1d957653b5615e22394ea62ff090ea841bf60
Alison Jarosz <alison.jarosz@viacesi.fr>
2023-12-20 03:16:00 +0100

    Solves Day 08

---
+++ Days/2023/08.cs
@@ -0,0 +1,35 @@
+﻿namespace Days._2023
+{
+    public static class HauntedWasteland
+    {
+        //"AAA = (BBB, BBB)",
+        private static readonly string[] separators = { " = (", ", ", ")" };
+        private static (List<char>, Dictionary<string, (string L, string R)>) Format(string[] input)
+        {
+            return (input[0].ToList(), input.Skip(2).Select(x =>
+            {
+                var test = x.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                return (test[0], (test[1], test[2]));
+            }).ToDictionary(x => x.Item1, x => x.Item2));
+        }
+
+        public static int GetStepsToZZZ(string[] input)
+        {
+            var (direction, nodes) = Format(input);
+
+            int i = 0;
+            var currNode = "AAA";
+            var dir = new Queue<char>(direction);
+            while (currNode != "ZZZ")
+            {
+                var nextDir = dir.Dequeue();
+                currNode = (nextDir == 'L') ? nodes[currNode].L : nodes[currNode].R;
+                i++;
+                if(dir.Count == 0) dir = new Queue<char>(direction);
+            }
+
+            return i;
+        }
+
+    }
+}
---
+++ Tests/2023/Day08.cs
@@ -0,0 +1,59 @@
+﻿using Days._2023;
+using Xunit;
+
+namespace Tests._2023
+{
+    public class Day08
+    {
+        private static readonly string[] test1 =
+        {
+            "RL",
+            "",
+            "AAA = (BBB, CCC)",
+            "BBB = (DDD, EEE)",
+            "CCC = (ZZZ, GGG)",
+            "DDD = (DDD, DDD)",
+            "EEE = (EEE, EEE)",
+            "GGG = (GGG, GGG)",
+            "ZZZ = (ZZZ, ZZZ)"
+        };
+        private static readonly string[] test2 =
+        {
+            "LLR",
+            "",
+            "AAA = (BBB, BBB)",
+            "BBB = (AAA, ZZZ)",
+            "ZZZ = (ZZZ, ZZZ)",
+        };
+
+        [Fact]
+        public void FirstStarExample()
+        {
+            Assert.Equal(2, HauntedWasteland.GetStepsToZZZ(test1));
+            Assert.Equal(6, HauntedWasteland.GetStepsToZZZ(test2));
+        }
+
+        [Fact]
+        public void FirstStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2023\\08.txt");
+            var result = HauntedWasteland.GetStepsToZZZ(dataset);
+            Assert.Equal(21409, result);
+        }
+
+        [Fact]
+        public void SecondStarExample()
+        {
+            Assert.Equal(2, HauntedWasteland.GetStepsToZZZ(test1));
+            Assert.Equal(6, HauntedWasteland.GetStepsToZZZ(test2));
+        }
+
+        [Fact]
+        public void SecondStarSolution()
+        {
+            var dataset = Days.Helpers.ReadFile("Inputs\\2023\\08.txt");
+            var result = HauntedWasteland.GetStepsToZZZ(dataset);
+            Assert.Equal(21409, result);
+        }
+    }
+}
