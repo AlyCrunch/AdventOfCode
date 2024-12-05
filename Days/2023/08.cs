5ac4ac2e611722550c020004dec9d76764600dab
Alison Jarosz <alison.jarosz@viacesi.fr>
2024-12-05 23:22:26 +0100

    Revert "Adds new year"
    This reverts commit 42218fe26d43dceeb225722609a9a4d1741186de.

--- Days/2023/08.cs
+++ Days/2023/08.cs
@@ -2,6 +2,7 @@
 {
     public static class HauntedWasteland
     {
+        //"AAA = (BBB, BBB)",
         private static readonly string[] separators = { " = (", ", ", ")" };
         private static (List<char>, Dictionary<string, (string L, string R)>) Format(string[] input)
         {
@@ -12,32 +13,23 @@
             }).ToDictionary(x => x.Item1, x => x.Item2));
         }
 
-        public static int GetStepsToZZZ(string[] input, string start = "AAA")
+        public static int GetStepsToZZZ(string[] input)
         {
             var (direction, nodes) = Format(input);
 
             int i = 0;
-            var currNode = start;
+            var currNode = "AAA";
             var dir = new Queue<char>(direction);
-            var end = start.Replace('A', 'Z');
-
-            while (currNode != end)
+            while (currNode != "ZZZ")
             {
                 var nextDir = dir.Dequeue();
                 currNode = (nextDir == 'L') ? nodes[currNode].L : nodes[currNode].R;
                 i++;
-                if (dir.Count == 0) dir = new Queue<char>(direction);
+                if(dir.Count == 0) dir = new Queue<char>(direction);
             }
+
             return i;
         }
 
-        public static int GetSimultaneousToZZZ(string[] input)
-        {
-            var (direction, nodes) = Format(input);
-            var currNode = nodes.Where(x => x.Key[^1] == 'A').Select(x => x.Key);
-            var results = currNode.Select(x => GetStepsToZZZ(input, x)).ToList();
-
-            return results.Skip(1).Aggregate(results.First(), (x, next) => LCM.Find(x, next));
-        }
     }
 }
--- Tests/2023/Day08.cs
+++ Tests/2023/Day08.cs
@@ -26,20 +26,6 @@ namespace Tests._2023
             "ZZZ = (ZZZ, ZZZ)",
         };
 
-        private static readonly string[] test3 =
-        {
-            "LR",
-            "",
-            "11A = (11B, XXX)",
-            "11B = (XXX, 11Z)",
-            "11Z = (11B, XXX)",
-            "22A = (22B, XXX)",
-            "22B = (22C, 22C)",
-            "22C = (22Z, 22Z)",
-            "22Z = (22B, 22B)",
-            "XXX = (XXX, XXX)",
-        };
-
         [Fact]
         public void FirstStarExample()
         {
@@ -51,20 +37,22 @@ namespace Tests._2023
         public void FirstStarSolution()
         {
             var dataset = Days.Helpers.ReadFile("Inputs\\2023\\08.txt");
-            Assert.Equal(21409, HauntedWasteland.GetStepsToZZZ(dataset));
+            var result = HauntedWasteland.GetStepsToZZZ(dataset);
+            Assert.Equal(21409, result);
         }
 
         [Fact]
         public void SecondStarExample()
         {
-            Assert.Equal(6, HauntedWasteland.GetSimultaneousToZZZ(test3));
+            Assert.Equal(2, HauntedWasteland.GetStepsToZZZ(test1));
+            Assert.Equal(6, HauntedWasteland.GetStepsToZZZ(test2));
         }
 
         [Fact]
         public void SecondStarSolution()
         {
             var dataset = Days.Helpers.ReadFile("Inputs\\2023\\08.txt");
-            var result = HauntedWasteland.GetSimultaneousToZZZ(dataset);
+            var result = HauntedWasteland.GetStepsToZZZ(dataset);
             Assert.Equal(21409, result);
         }
     }
